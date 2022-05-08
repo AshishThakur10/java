5a70f6acc02e25a3c106e2c95c51b6e73e00347b
AshishThakur10 <sendashishthakur@gmail.com>
2022-05-08 08:54:30 +0530

    even or odd

---
+++ util/NumberIsEvenOrOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class NumberIsEvenOrOdd{
+    public static void main(String[] args) {
+       int x;
+       Scanner sc = new Scanner(System.in);
+       x = sc.nextInt();
+       if(x%2 == 0){
+           System.out.println("Even number " + x);
+       } else{
+           System.out.println("Odd number " + x);
+       }
+    }
+}
