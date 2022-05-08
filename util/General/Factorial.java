d9344af9319c4807480e2f7dfa3d04ced272a08e
AshishThakur10 <sendashishthakur@gmail.com>
2022-05-08 16:59:52 +0530

    factorial

---
+++ util/General/Factorial.java
@@ -0,0 +1,18 @@
+package General;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n, fact = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        n = sc.nextInt();
+        for(int i = 1; i<= n; i++)
+        {
+            fact = fact*i;
+            System.out.println(fact);
+        }
+        sc.close();
+    }
+}
