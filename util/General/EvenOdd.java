182ffe725fb8622a49dabba1573dd6738945f748
AshishThakur10 <sendashishthakur@gmail.com>
2022-05-22 20:00:24 +0530

    even odd

---
+++ util/General/EvenOdd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class EvenOdd {
+    //question- to check number is even or odd 
+    
+    public static void main(String[] args) {
+        
+        int number;
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the number");
+        
+        number = input.nextInt();
+
+        if (number % 2 == 0) {
+        System.out.println("Number is even");           
+        }
+
+        else{
+        System.out.println("Number is odd");           
+        }
+
+        
+    }
+}
