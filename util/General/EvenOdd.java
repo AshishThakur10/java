a16f08df9251f37570dac17228415d4886e80c4d
AshishThakur10 <sendashishthakur@gmail.com>
2022-05-24 21:50:33 +0530

    fix

--- util/General/EvenOdd.java
+++ util/General/EvenOdd.java
@@ -13,6 +13,7 @@ public class EvenOdd {
         System.out.println("Enter the number");
         
         number = input.nextInt();
+        input.close();
 
         if (number % 2 == 0) {
         System.out.println("Number is even");           
--- util/General/PrimeNum.java
+++ util/General/PrimeNum.java
@@ -9,6 +9,7 @@ public class PrimeNum {
 
         Scanner input = new Scanner(System.in);
         number = input.nextInt();
+        input.close();
 
         for(int i = 1; i<= number; i++){
             if(number % i == 0){
